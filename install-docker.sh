49922cbd35a1109b628051b089289893eb6ffc6e
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2021-09-02 01:01:00 +0200

    install docker on ubuntu

---
+++ install-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum -y update && yum -y upgrade
+yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+yum install -y yum-utils vim nano wget curl
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --enable docker-ce-nightly
+yum-config-manager --enable docker-ce-test
+yum-config-manager --disable docker-ce-nightly
+yum -y install docker-ce docker-ce-cli containerd.io
+yum -y install docker-compose
