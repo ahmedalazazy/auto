47165d081f4316cb1dccade85f6ec1b96314c633
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2021-10-27 21:18:17 +0200

    Update change_Disk_type.sh

--- change_Disk_type.sh
+++ change_Disk_type.sh
@@ -16,13 +16,16 @@ gcloud compute instances stop $INSTANCE_NAME --zone "$ZONE";
 echo "vm in stoping process";
 gcloud compute disks snapshot "$ORGDISK_NAME" --zone=us-central1-a --snapshot-names="$DISK_NAME";
 echo "tacke snapshot in process";
+#if you need to change disk type you can edit --type="name of disk type" or make it my var
 gcloud compute disks create "$NEW_DISK" --source-snapshot "$DISK_NAME" --type="pd-standard" --zone "$ZONE";
 echo "new DISK in creating process";
 gcloud beta compute instances detach-disk "$INSTANCE_NAME" --disk "$ORGDISK_NAME" --zone "$ZONE";
 echo "detaching old disk in process"
+#if thes disk is a boot vm disk please add --boot in the below line 
 gcloud beta compute instances attach-disk "$INSTANCE_NAME" --disk "$NEW_DISK" --zone "$ZONE";
 echo "attach new disk in process";
 read -p "to keep vm stoped type 0 to start vm type 1  : " STOPSTART;
+#this if to ask you if you not need to take more actions in thes vm start it if you stell need more actions keep it stoped
 if [[ $STOPSTART -eq "1" ]]; then
 	gcloud compute instances start $INSTANCE_NAME;
 	echo "vm started";
