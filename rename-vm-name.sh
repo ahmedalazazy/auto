a8b5d5a479a44533662ff99c94fbe964c09dade4
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2021-11-11 12:05:19 +0200

    rename-vm-name.sh
    This script to rename Instance on GCP
    #GCP
    #VM

--- rename-vm-name.sh
+++ rename-vm-name.sh
@@ -19,9 +19,9 @@ echo "This process will stop INSTANS";
 read -p "Please Enter Instans Name" CURRENT_NAME;
 read -p "Please Enter New Instans Name" NEW_NAME;
 read -p "Please Enter Instans ZONE" ZONE;
-gcloud compute instances stop "$CURRENT_NAME" -—zone="$ZONE"
+gcloud compute instances stop "$CURRENT_NAME" --zone "$ZONE"
 echo "VM In Stoping process"
-gcloud beta compute instances set-name "$CURRENT_NAME" -—zone="$ZONE" -—new-name="$NEW_NAME"
+gcloud beta compute instances set-name "$CURRENT_NAME" --zone "$ZONE" --new-name="$NEW_NAME"
 echo "VM In rename process"
-gcloud compute instances start "$NEW_NAME" -—zone="$ZONE"
+gcloud compute instances start "$NEW_NAME" --zone="$ZONE"
 echo "VM started with New Name $NEW_NAME"
