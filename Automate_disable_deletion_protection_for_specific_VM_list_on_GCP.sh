bc50517a9a83a3bdde989db85091d7521ab0d005
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2021-12-22 16:06:48 +0200

    Automate disable deletion protection for specific VM list
    Automate disable deletion protection for specific VM list on GCP

---
+++ Automate_disable_deletion_protection_for_specific_VM_list_on_GCP.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash"
+
+RED='\033[01;31m'
+RESET='\033[0m'
+GREEN='\033[01;32m'
+
+clear
+echo -e "                        \t   $RED Hi $USER $RESET"
+sleep 2
+echo -e "$GREEN**********************************************************************************$RESET"
+sleep 1
+echo -e "  				This Script for Automate  disnable deletion protection for an instance **$RESET"              
+sleep 5
+echo -e "$GREEN*****************************Github: ahmedalazazy*********************************$RESET"
+sleep 2
+echo -e " \t $RED Please $USER add VMs and zone to file and make name of the file=file00.txt     $RESET"
+sleep 5
+echo -e " \t $RED Please $USER and make syntax NAME and zone in this file like this : ServerNAME,SERVERZONE $RESET"
+sleep 5
+echo -e "$GREEN**********************************************************************************$RESET"
+echo ""
+echo -e "                        \t   $RED Hi $USER $RESET"
+echo ""
+read -p "Please enter project you need to disnable deletion protection under it : " PROJECTT;
+gcloud config set project "$PROJECTT"
+echo "Project set to "$PROJECTT""
+while IFS=, read NAME ZONE; do
+
+	gcloud compute instances update "$NAME" --no-deletion-protection --zone="$ZONE" --project="$PROJECTT"
+
+done <<< $( cat file00.txt)
