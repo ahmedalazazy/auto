f8319bba91bbd5716eea69bb2b2d9b9d01884fec
ahmedalazazy <ahmedal3zazy900@gmail.com>
2023-02-15 15:28:10 +0200

    add create mi to anther account

--- create-sn-mi-from-list.py
+++ create-sn-mi-from-list.py
@@ -147,7 +147,77 @@ elif sys.argv[1:][0] == 'add-resource-policies-to-disks':
                 Disk_Policy = row[1]
                 DISK_ZONE = row[2]
                 project_id = row[3]
-                subprocess.Popen(['/bin/bash', '-c', f'gcloud compute disks add-resource-policies {DISK_NAME} --resource-policies={Disk_Policy} --zone={DISK_ZONE} --project={project_id}'])
+                subprocess.run(['/bin/bash', '-c', f'gcloud compute disks add-resource-policies {DISK_NAME} --resource-policies={Disk_Policy} --zone={DISK_ZONE} --project={project_id}'])
+                
+#####################################################                  
+elif sys.argv[1:][0] == 'remove-resource-policies-to-disks':
+    print('welcome to remove remove-resource-policies to disks')
+    filename = sys.argv[1:][1]+".csv"
+    with open(filename, 'r') as csvfile_tow:
+            datareader_tow = csv.reader(csvfile_tow)
+            for row in datareader_tow:
+                DISK_NAME = row[0]
+                DISK_ZONE = row[1]
+                project_id = row[2]
+                subprocess.Popen(['/bin/bash', '-c', f'OLDBOOT_DISK_LICENSE=$(gcloud beta compute disks describe {DISK_NAME} --zone={DISK_ZONE} --project={project_id} --format="csv[no-heading](resourcePolicies.basename())") && gcloud compute disks remove-resource-policies {DISK_NAME} --resource-policies=$OLDBOOT_DISK_LICENSE --zone={DISK_ZONE} --project={project_id}'])
+#####################################################                  
+elif sys.argv[1:][0] == 'remove-sn-schdule-from-disks-and-delete-sn':
+    print('welcome to remove resource-policies ad snapshots for disks')
+    filename = sys.argv[1:][1]+".csv"
+    with open(filename, 'r') as csvfile_tow:
+            datareader_tow = csv.reader(csvfile_tow)
+            for row in datareader_tow:
+                DISK_NAME = row[0]
+                DISK_ZONE = row[1]
+                project_id = row[2]
+                subprocess.run(['/bin/bash', '-c', f'OLDBOOT_DISK_LICENSE=$(gcloud beta compute disks describe {DISK_NAME} --zone={DISK_ZONE} --project={project_id} --format="csv[no-heading](resourcePolicies.basename())") && gcloud compute disks remove-resource-policies {DISK_NAME} --resource-policies=$OLDBOOT_DISK_LICENSE --zone={DISK_ZONE} --project={project_id}'])
+                cmd = f'gcloud compute snapshots list --filter="sourceDisk={DISK_NAME}" --format="value(name)" --project={project_id}'
+                output = subprocess.check_output(cmd, shell=True).decode('utf-8')
+                snapshots = output.split('\n')[:-1]  # remove last empty item
+                print(f'Found {len(snapshots)} snapshots for disk {DISK_NAME}')
+                print(snapshots)
+                with open(f'{project_id}-snapshots.csv', 'a', newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([DISK_NAME, len(snapshots)] + snapshots)
+                for snapshot in snapshots:
+                    cmd2 = f'gcloud compute snapshots delete {snapshot} --quiet --project={project_id}'
+                    subprocess.run(cmd2, shell=True)
+                    print(f'Deleted {len(snapshots)} snapshots for disk {DISK_NAME}')
+                with open(f'{project_id}-deleted-snapshots.csv', 'a', newline='') as csvfile2:
+                    writer = csv.writer(csvfile2)
+                    writer.writerow([DISK_NAME, len(snapshots)] + snapshots)
+
+#####################################################
+elif sys.argv[1:][0] == 'create-mi-to-anther-project':
+    print('welcome to create mi to anther project')
+    Source_Project_id = input("Please Enter SORCE PROJECT ID:")
+    target_project_id = input("Please Enter TARGET PROJECT ID:")
+    cmd = f'gcloud compute instances list --format="csv[no-heading](NAME,ZONE)" --project={Source_Project_id}'
+    output = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    InstancesLIST = output.split('\n')[:-1]  # remove last empty item
+    VMS = csv.reader(InstancesLIST)
+    for VM in VMS:
+        VM_NAME = VM[0]
+        VM_ZONE = VM[1]
+        MI_NAME = f'{VM_NAME}-moved-from-{Source_Project_id}'
+        # define the fixed column width
+        column_width = 30
+        # format the variables with a fixed width
+        vm_name_TABLE = '{:<{width}}'.format(VM_NAME, width=column_width)
+        source_project_id_TABLE = '{:<{width}}'.format(Source_Project_id, width=column_width)
+        target_project_id_TABLE = '{:<{width}}'.format(target_project_id, width=column_width)
+        cmd2 = f'gcloud compute machine-images create {MI_NAME} --source-instance="projects/{Source_Project_id}/zones/{VM_ZONE}/instances/{VM_NAME}"  --project={target_project_id}'
+        subprocess.run(cmd2, shell=True)
+        LOGS = f'| Machine - image create from VM: {vm_name_TABLE} | sorce Project: {source_project_id_TABLE} | in target Project: {target_project_id_TABLE} |'
+        LOGST = f'Machine - image create from VM: {vm_name_TABLE},sorce Project: {source_project_id_TABLE},in target Project: {target_project_id_TABLE},'
+        LOGSFILEPATH = f'LOGS-for-Created-MI-IN-{target_project_id}-from-{Source_Project_id}.csv'
+        print(f'{LOGS}')
+#        with open(f'{LOGSFILEPATH}', 'a', newline='') as csvfile:
+#             writer = csv.writer(csvfile)
+#             writer.writerow(f'{LOGST}')        
+
+
+
 
 #####################################################
 elif sys.argv[1:][0] == 'add-tags':
--- migrate-ips-to-new-project.py
+++ migrate-ips-to-new-project.py
@@ -1,6 +1,7 @@
-#!/bin/python3
 import csv, subprocess, sys, os
-print("This script to Migrate IPS to anther Project and re asine to migrated machines ")
+
+print(f'This script to Migrate IPS to anther Project and re asine to migrated machines')
+
 OR_PROJECT = input("PLease Type the Origenal project ID: ")
 NEW_PROJECT = input("PLease Type the NEW PROJECT project ID: ")
 filename = f"{OR_PROJECT}-ex-ips-to-move-{NEW_PROJECT}.csv"
@@ -12,7 +13,7 @@ with open(filename, 'r') as csvfile:
         ADDRESS_NAME = row[0]
         REGION = row[1]
         ADDRESS_RANGE = row[2]
-        subprocess.run(['/bin/bash', '-c', f'VMNAME=$(gcloud compute addresses describe {ADDRESS_NAME} --project={OR_PROJECT} --format="csv[no-heading](users)") && VMZONE=$(gcloud compute instances describe "$VMNAME" --format="csv[no-heading](zone.scope())" --project={OR_PROJECT}) && gcloud compute instances delete-access-config "$VMNAME"--zone="$VMZONE"  --access-config-name="External NAT" --project={OR_PROJECT} '])
+        subprocess.run(['/bin/bash', '-c', f'VMNAME=$(gcloud compute addresses describe {ADDRESS_NAME} --project={OR_PROJECT} --format="csv[no-heading](users)") && VMZONE=$(gcloud compute instances describe "$VMNAME" --format="csv[no-heading](zone.scope())" --project={OR_PROJECT}) && gcloud compute instances delete-access-config "$VMNAME" --zone="$VMZONE"  --access-config-name="External NAT" --project={OR_PROJECT} '])
         subprocess.run(['/bin/bash', '-c', f'gcloud alpha compute addresses move {ADDRESS_NAME} --target-project={NEW_PROJECT} --region={REGION} --project={OR_PROJECT}'])
         subprocess.run(['/bin/bash', '-c', f'VMNAME=$(gcloud compute addresses describe {ADDRESS_NAME} --project={OR_PROJECT} --format="csv[no-heading](users)") && VMZONE=$(gcloud compute instances describe "$VMNAME" --format="csv[no-heading](zone.scope())" --project={OR_PROJECT}) && gcloud compute instances add-access-config $VMNAME --access-config-name="External NAT" --address={ADDRESS_RANGE} --project={NEW_PROJECT} '])
 
