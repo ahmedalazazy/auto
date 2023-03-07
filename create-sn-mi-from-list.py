cf49b8141c18cd3548e81a8c5ff2091cc0e3cfd4
ahmedalazazy <ahmedal3zazy900@gmail.com>
2023-03-07 13:06:44 +0200

    move-fms-out-of-sole

--- create-sn-mi-from-list.py
+++ create-sn-mi-from-list.py
@@ -255,6 +255,26 @@ elif sys.argv[1:][0] == 'rm-tags':
             project_id = row[3] 
             subprocess.run(['/bin/bash', '-c', f'gcloud compute instances remove-tags {vm_name} --zone={vm_zone} --tags={TAG} --project={project_id}'])
             print(f'{vm_name} tag {TAG} removed' )
+##########################################################
+elif sys.argv[1:][0] == 'move-vms-out-of-sole':
+    project_id = input("Please Enter the PROJECT ID:")
+    # Run the gcloud command to list all the firewall rules in the project
+    list_command = f'gcloud compute instances list  --format="csv[no-heading](NAME,ZONE,scheduling.nodeAffinities[values])" --project={project_id}'
+    output = subprocess.check_output(list_command, shell=True).decode('utf-8')
+    InstancesLIST = output.split('\n')[:-1]  # remove last empty item
+    VMS = csv.reader(InstancesLIST)
+
+    for VM in VMS:
+        VM_NAME = VM[0]
+        VM_ZONE = VM[1]
+        NODE_AFFINITES = VM[2]
+        if NODE_AFFINITES:
+            cmd2 = f'gcloud compute instances set-scheduling "{VM_NAME}" --clear-node-affinities --zone={VM_ZONE} --project={project_id}'
+            subprocess.run(cmd2, shell=True)
+            print(f'{VM_NAME}-{VM_ZONE}-{project_id}-out - of the sole now')
+            cmd3 = f'gcloud compute instances start "{VM_NAME}" --zone={VM_ZONE} --project={project_id}'
+            subprocess.run(cmd3, shell=True)
+            print(f'{VM_NAME}-{VM_ZONE}-{project_id}-started')
 ############################################################
 else:
     print('coming soon')
---
+++ remove-vms-out-soltancy.py
@@ -0,0 +1,20 @@
+import csv, subprocess, sys, os
+
+project_id = input("Please Enter the PROJECT ID:")
+# Run the gcloud command to list all the firewall rules in the project
+list_command = f'gcloud compute instances list  --format="csv[no-heading](NAME,ZONE,scheduling.nodeAffinities[values])" --project={project_id}'
+output = subprocess.check_output(list_command, shell=True).decode('utf-8')
+InstancesLIST = output.split('\n')[:-1]  # remove last empty item
+VMS = csv.reader(InstancesLIST)
+
+for VM in VMS:
+    VM_NAME = VM[0]
+    VM_ZONE = VM[1]
+    NODE_AFFINITES = VM[2]
+    if NODE_AFFINITES:
+        cmd2 = f'gcloud compute instances set-scheduling "{VM_NAME}" --clear-node-affinities --zone={VM_ZONE} --project={project_id}'
+        subprocess.run(cmd2, shell=True)
+        print(f'{VM_NAME}-{VM_ZONE}-{project_id}-out - of the sole now')
+        cmd3 = f'gcloud compute instances start "{VM_NAME}" --zone={VM_ZONE} --project={project_id}'
+        subprocess.run(cmd3, shell=True)
+        print(f'{VM_NAME}-{VM_ZONE}-{project_id}-started')
