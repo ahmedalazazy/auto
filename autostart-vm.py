022ff516e3e94182bd4f5a1294ef4d27a0e053b1
ahmedalazazy <ahmedal3zazy900@gmail.com>
2023-06-08 13:41:50 +0300

    add-autostart-vm0.2

--- autostart-vm.py
+++ autostart-vm.py
@@ -6,10 +6,11 @@ import time
 # Prompt the user to enter the project ID and VM instance name
 project_id = input("Enter the project ID: ")
 instance_name = input("Enter the VM instance name: ")
-
+ZONE_NAME = input("Enter the VM instance zone: ")
+web_hook = input("Enter the web_hook url for status : ")
 def check_vm_status(project_id, instance_name):
     # Run gcloud command to get the status of the VM instance
-    command = ["gcloud", "compute", "instances", "describe", instance_name, "--project", project_id, "--format=value(status)"]
+    command = ["gcloud", "compute", "instances", "describe", instance_name, "--project", project_id, "--format=value(status)", "--zone", ZONE_NAME]
     result = subprocess.run(command, capture_output=True, text=True)
     
     # Extract the status from the command output
@@ -19,8 +20,25 @@ def check_vm_status(project_id, instance_name):
 
 def start_vm_instance(project_id, instance_name):
     # Run gcloud command to start the VM instance
-    command = ["gcloud", "compute", "instances", "start", instance_name, "--project", project_id]
+    command = ["gcloud", "compute", "instances", "start", instance_name, "--project", project_id, "--zone", ZONE_NAME]
     subprocess.run(command)
+
+
+# send a message if vm started
+def web_hook_trigger(msg):
+    url = os.getenv('web_hook')
+    payload = {
+        "channel":"#scripts_alerts",
+        "text": f"{msg}",
+        "username":"webhookbot",
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    print(response.text.encode('utf8'))
+
+
 
 
 while True:
@@ -30,6 +48,7 @@ while True:
     if status != "RUNNING":
         print("VM instance is not running. Starting it now...")
         start_vm_instance(project_id, instance_name)
+        
     else:
         print("VM instance is running.")
         break
