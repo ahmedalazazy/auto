d05a73aaa09110d0a81d5f0e9137bb42d4c5eaff
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2023-07-18 00:55:28 +0300

    Update auto-devportal.sh

--- auto-devportal.sh
+++ auto-devportal.sh
@@ -394,47 +394,8 @@ function nginxconfigration() {
     fi
     sudo find /etc/nginx/conf.d/ -maxdepth 1 -type f -name "*.conf" -exec mv -n {} {}.bkp \;
     sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bkp
-
-sudo bash -c 'cat << EOF > /etc/nginx/nginx.conf
-user nginx;
-worker_processes auto;
-error_log /var/log/nginx/error.log;
-pid /run/nginx.pid;
-
-# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
-include /usr/share/nginx/modules/*.conf;
-
-events {
-    worker_connections 1024;
-}
-
-http {
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
-
-    access_log  /var/log/nginx/access.log  main;
-
-    sendfile            on;
-    tcp_nopush          on;
-    tcp_nodelay         on;
-    keepalive_timeout   65;
-    types_hash_max_size 2048;
-
-    include             /etc/nginx/mime.types;
-    default_type        application/octet-stream;
-
-    # Load modular configuration files from the /etc/nginx/conf.d directory.
-    # See http://nginx.org/en/docs/ngx_core_module.html#include
-    # for more information.
-    include /etc/nginx/conf.d/*.conf;
-}
-EOF'
-
-
-
-    curl https://raw.githubusercontent.com/ahmedalazazy/auto/main/nginxconfigration -o /etc/nginx/conf.d/drupal-nginx.conf
-
+    sudo curl https://raw.githubusercontent.com/ahmedalazazy/auto/main/nginxrhel  -o /etc/nginx/nginx.conf
+    sudo curl https://raw.githubusercontent.com/ahmedalazazy/auto/main/nginxconfigration -o /etc/nginx/conf.d/drupal-nginx.conf
     sudo systemctl restart nginx.service
     NGNGNGSTATUS=$(sudo systemctl status nginx.service )
     if echo "$NGNGNGSTATUS" | grep -q "running" ; then
