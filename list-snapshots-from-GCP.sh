0b3bc4f8f1dc1bd99820f959ec3a0ead991330bd
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2023-07-18 10:44:14 +0300

    Create list-snapshots-from-GCP.sh

---
+++ list-snapshots-from-GCP.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompt the user to enter the project ID
+read -p "Enter the project ID: " PROJECT_ID
+
+# Use the PROJECT_ID variable in the gcloud command
+gcloud compute snapshots list \
+  --format="csv(name,creation_timestamp,disk_size_gb,storage_bytes,storage_locations,SRC_DISK.basename(),status)" \
+  --project=${PROJECT_ID} \
+  > ${PROJECT_ID}/${PROJECT_ID}_snapshots.csv
