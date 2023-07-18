4cb0545949df3dd33bbb6b7913dfde1b89e8a9ea
ahmedalazazy <51747858+ahmedalazazy@users.noreply.github.com>
2023-07-18 10:49:59 +0300

    Update list-snapshots-from-GCP.sh

--- list-snapshots-from-GCP.sh
+++ list-snapshots-from-GCP.sh
@@ -6,5 +6,4 @@ read -p "Enter the project ID: " PROJECT_ID
 # Use the PROJECT_ID variable in the gcloud command
 gcloud compute snapshots list \
   --format="csv(name,creation_timestamp,disk_size_gb,storage_bytes,storage_locations,SRC_DISK.basename(),status)" \
-  --project=${PROJECT_ID} \
-  > ${PROJECT_ID}/${PROJECT_ID}_snapshots.csv
+  --project=${PROJECT_ID} > ${PROJECT_ID}_snapshots.csv
